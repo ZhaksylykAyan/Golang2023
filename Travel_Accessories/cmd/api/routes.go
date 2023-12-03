7d625ed3f51c61a24f38a230af9fdeb3d22fc64f
Ayan <affection.ayan@mail.com>
2023-12-03 17:38:09 +0600

    finished ch16

--- Travel_Accessories/cmd/api/routes.go
+++ Travel_Accessories/cmd/api/routes.go
@@ -18,6 +18,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodDelete, "/v1/accessories/:id", app.deleteAccessoryHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
-	return app.recoverPanic(app.rateLimit(router))
+	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
 }
